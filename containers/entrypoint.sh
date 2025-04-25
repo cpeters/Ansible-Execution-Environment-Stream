bfa92ac5f6f71a205c0a88d2a5a1e36ac82b5056
Chris Peters <chpeters@redhat.com>
2025-04-25 12:06:04 -0400

    Add working container file and move others to scratch dir

--- containers/entrypoint.sh
+++ containers/entrypoint.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
-#if [ ! -d "${HOME}" ]
-#then
-#  mkdir -p "${HOME}"
-#fi
-#if ! whoami &> /dev/null
-#then
-#  if [ -w /etc/passwd ]
-#  then
-#    echo "${USERNAME:-user}:x:$(id -u):0:${USERNAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
-#    echo "${USERNAME:-user}:x:$(id -u):" >> /etc/group
-#  fi
-#fi
 USER=$(whoami)
-START_ID=$(( $(id -u)+1 ))
-END_ID=$(( 65536-${START_ID} ))
-echo "${USER}:${START_ID}:${END_ID}" > /etc/subuid
-echo "${USER}:${START_ID}:${END_ID}" > /etc/subgid
+START_ID=$(($(id -u) + 1))
+END_ID=$((65536 - ${START_ID}))
+echo "${USER}:${START_ID}:${END_ID}" >/etc/subuid
+echo "${USER}:${START_ID}:${END_ID}" >/etc/subgid
 /usr/libexec/podman/catatonit -- "$@"
